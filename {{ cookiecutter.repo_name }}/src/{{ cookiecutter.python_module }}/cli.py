6644019e9ee80fd9fc6309c3b97cdd5a83ba34cd
Per Unneberg <per.unneberg@scilifelab.se>
2022-06-12 22:47:29 +0200

    Update cli module

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_module }}/cli.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_module }}/cli.py
@@ -40,88 +40,105 @@ class DescriptionArgumentParser(ArgumentParser):
         self.exit(2, "%(prog)s: error: %(message)s\n" % args)
 
 
-def get_project_parser(arg_list=None):
-    if arg_list is None:
-        arg_list = sys.argv[1:]
-
-    parser = DescriptionArgumentParser(description=__doc__, prog="{{ cookiecutter.project_name }}")  # noqa
-    parser.add_argument(
-        "--version", action="version", version="%(prog)s " + __version__
+def get_project_parser():
+    # fmt: off
+    top_parser = DescriptionArgumentParser(
+        description=__doc__,
+        prog="{{ cookiecutter.project_name }}"
     )
-    parser.add_argument(
+    top_parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s " + __version__
+    )
+    top_parser.add_argument(
         "--debug",
         action="store_true",
         default=False,
         help="Print debug messages",
     )
-    return parser, arg_list
-
-
-def add_project_subcommand_parser(parser, arg_list=None):
-    subcommand_name = get_subcommand_name(arg_list)
-    module = importlib.import_module(
-        "." + subcommand_name, subcommands.__name__
-    )  # noqa
-    subparsers = parser.add_subparsers()
-    subparser = subparsers.add_parser(
-        subcommand_name,
-        help=module.__doc__.split("\n", maxsplit=1)[1],
-        description=module.__doc__,
-    )
-    module.add_arguments(subparser)
-    return parser
+    # fmt: on
+    return top_parser
 
 
 def main(arg_list=None):
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")  # noqa
-    parser, arg_list = get_project_parser(arg_list)
+    if arg_list is None:
+        arg_list = sys.argv[1:]
+        logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    try:
+        from nbis import subcommands as nbis_subcommands
+
+        package_list = [subcommands, nbis_subcommands]
+    except ModuleNotFoundError:
+        package_list = [subcommands]
+    minimal_parser, subcommands_map = make_minimal_parser(package_list)
+    subcommand_name = get_subcommand_name(minimal_parser, arg_list)
+    # fmt: off
+    parser = make_subcommand_parser(
+        subcommand_name, subcommands_map[subcommand_name]
+    )
+    # fmt: on
+
     args = parser.parse_args(arg_list)
 
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
     del args.debug
 
-    try:
-        from nbis.cli import add_subcommand_parser
+    args.runner(args)
 
-        parser = add_subcommand_parser(parser, arg_list)
-    except Exception as e:
-        logger.debug(e)
-
-    parser = add_project_subcommand_parser(parser, arg_list=None)
-
-    args = parser.parse_args(arg_list)
+    return 0
 
 
-def get_subcommand_name(arguments) -> str:
-    """Parse arguments to find out which subcommand was requested.
-
-    This sets up a minimal ArgumentParser with the correct help
-    strings.
-
-    Because help is obtained from a module’s docstring, but importing
-    each module makes startup slow, the modules are only parsed with
-    the ast module and not fully imported at this stage.
-
-    Return:
-        subcommand name
-
-    """
-    parser = DescriptionArgumentParser(description=__doc__, prog="{{ cookiecutter.project_name }}")  # noqa
-    parser.add_argument("--version", action="version", version=__version__)
-    subparsers = parser.add_subparsers()
-
-    for module_name, docstring in subcommands_modules(subcommands):
-        hstring = docstring.split("\n", maxsplit=2)[1].replace("%", "%%")
-        subparser = subparsers.add_parser(
-            module_name, help=hstring, description=docstring, add_help=False
-        )
-        subparser.set_defaults(module_name=module_name)
-    args, _ = parser.parse_known_args(arguments)
+def get_subcommand_name(parser, arg_list) -> str:
+    """Parse arg_list to get name of subcommand"""
+    args, _ = parser.parse_known_args(arg_list)
     module_name = getattr(args, "module_name", None)
     if module_name is None:
         parser.error("Please provide the name of a subcommand to run")
     return module_name
+
+
+def make_subcommand_parser(subcommand_name, package):
+    """
+    Make parser and load module whilst adding subparser documentation.
+    """
+    parser = get_project_parser()
+    subparsers = parser.add_subparsers()
+    module = importlib.import_module("." + subcommand_name, package.__name__)
+    subparser = subparsers.add_parser(
+        subcommand_name,
+        help=module.__doc__.split("\n", maxsplit=1)[1],
+        description=module.__doc__,
+    )
+    subparser.set_defaults(module_name=subcommand_name, runner=module.main)
+    module.add_arguments(subparser)
+    return parser
+
+
+# We need the minimal parser just to get the actual subcommand name
+def make_minimal_parser(package_list):
+    """
+    Make minimal parser including subcommands from package_list
+    """
+    parser = get_project_parser()
+    subparsers = parser.add_subparsers()
+    subcommands_map = dict()
+
+    if not isinstance(package_list, list):
+        package_list = [package_list]
+    for pkg in package_list:
+        for module_name, docstring in subcommands_modules(pkg):
+            subcommands_map[module_name] = pkg
+            help_str = docstring.split("\n", maxsplit=1)[1].replace("%", "%%")
+            # fmt: off
+            subparser = subparsers.add_parser(
+                module_name, help=help_str,
+                description=docstring, add_help=False
+            )
+            # fmt: on
+        subparser.set_defaults(module_name=module_name)
+    return parser, subcommands_map
 
 
 def subcommands_modules(package):
