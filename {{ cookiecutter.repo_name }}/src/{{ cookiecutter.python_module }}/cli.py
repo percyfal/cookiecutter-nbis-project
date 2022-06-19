f4df5c5f783927efd1e661177ef99934f3e84ac8
Per Unneberg <per.unneberg@scilifelab.se>
2022-06-19 20:37:08 +0200

    Use nbis-admin top_parser to init parser

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_module }}/cli.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_module }}/cli.py
@@ -12,27 +12,6 @@ __author__ = "Per Unneberg"
 logger = logging.getLogger(__name__)
 
 
-def get_project_parser():
-    # fmt: off
-    top_parser = cli.DescriptionArgumentParser(
-        description=__doc__,
-        prog="{{ cookiecutter.python_module }}"
-    )
-    top_parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s " + __version__
-    )
-    top_parser.add_argument(
-        "--debug",
-        action="store_true",
-        default=False,
-        help="Print debug messages",
-    )
-    # fmt: on
-    return top_parser
-
-
 def main(arg_list=None):
     if arg_list is None:
         arg_list = sys.argv[1:]
@@ -43,10 +22,10 @@ def main(arg_list=None):
         package_list = [subcommands, nbis_subcommands]
     except ModuleNotFoundError:
         package_list = [subcommands]
-    top_parser = get_project_parser()
+    top_parser = cli.get_top_parser("{{ cookiecutter.python_module }}", __version__)
     minimal_parser, subcommands_map = cli.make_minimal_parser(top_parser, package_list)
     subcommand_name = cli.get_subcommand_name(minimal_parser, arg_list)
-    top_parser = get_project_parser()
+    top_parser = cli.get_top_parser("{{ cookiecutter.python_module }}", __version__)
     # fmt: off
     parser = cli.make_subcommand_parser(
         top_parser, subcommand_name, subcommands_map[subcommand_name]
